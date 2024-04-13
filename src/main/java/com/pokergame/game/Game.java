055a57168929fb384e26a37dcdcdd311b2ecd147
Schalk <pretorius.scw@gmail.com>
2024-04-13 14:41:20 +0200

    adding game logic layout

--- src/main/java/com/pokergame/game/Game.java
+++ src/main/java/com/pokergame/game/Game.java
@@ -1,4 +1,43 @@
 package com.pokergame.game;
 
+import com.pokergame.model.Player;
+
+import java.util.List;
+
 public class Game {
+    private java.util.List<Player> players;
+    private boolean gameEnded;
+
+    public Game(PokerVariant pokerVariant) {
+        this.players = new java.util.ArrayList<>();
+        this.gameEnded = false;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void startGame() {
+    }
+
+    public void playRound() {
+    }
+
+    public void determineWinner() {
+    }
+
+    public void endGame() {
+        for (Player player : players) {
+            player.clearHand();
+        }
+        this.gameEnded = true;
+    }
+
+    public boolean isOver() {
+        return this.gameEnded;
+    }
 }
