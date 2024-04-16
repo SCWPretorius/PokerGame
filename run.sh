60f2dc78fd079762d13b5b9cc8d1df22c14b123b
Schalk <pretorius.scw@gmail.com>
2024-04-16 10:07:37 +0200

    added script

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 POKER="poker-1.0.jar"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
-if [ ! -f "./target/poker-1.0.jar" ]
+if [ ! -f "./target/$POKER" ]
 then
    mvn clean validate test package
 fi
 
-if [ -f "./target/poker-1.0.jar" ]
+if [ -f "./target/$POKER" ]
 then
    cd "$DIR/target"
-   java -jar $POKER
+   java -jar $POKER --enable-preview
 fi
